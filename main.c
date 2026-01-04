5e3a0ef3b356bfe6b49aaed181da87a6eeae79b3
cyanyaan <ankitsarkar431@proton.me>
2026-01-04 20:18:35 +0530

    ok

--- main.c
+++ main.c
@@ -18,21 +18,25 @@ typedef struct{
 } Player;
 
 void drawMap(int mapWidth, int mapHeight, customCamera *cam){
-    for (int x = 0; x < mapWidth; x+=64)
-    {
-        for (int y = 0; y < mapHeight; y+=64)
-        {
-            DrawRectangle(x-cam->position.x + cam->offset.x, y-cam->position.y + cam->offset.x, 64,64, GREEN);
-        } 
-    }
+    DrawRectangle(
+        (0 - cam->position.x) * cam->zoom + cam->offset.x,
+        (0 - cam->position.y) * cam->zoom + cam->offset.y,
+        mapWidth  * cam->zoom,
+        mapHeight * cam->zoom,
+        GREEN
+    );
+        
 }
 
 void drawPlayer(Player *player, customCamera *cam){
-    DrawRectangle(player->position.x - cam->position.x + cam->offset.x, player->position.y - cam->position.y + cam->offset.y, player->size, player->size, player->color);
+    DrawRectangle((player->position.x - cam->position.x) * cam->zoom  + cam->offset.x,
+                  (player->position.y - cam->position.y) * cam->zoom  + cam->offset.y,
+                   player->size * cam->zoom , player->size * cam->zoom ,
+                   player->color);
 }
 
 int main(){
-    InitWindow(SCREENWIDTH, SCREENHEIGHT, "custom camera");
+    InitWindow(SCREENWIDTH, SCREENHEIGHT, "camera");
     SetTargetFPS(60);
 
     int mapWidth = 1000;
@@ -49,6 +53,9 @@ int main(){
     cam.offset = (Vector2){SCREENWIDTH/2.0f, SCREENHEIGHT/2.0f};
     cam.zoom = 1.0f;
 
+    Vector2 lastMousePos = {0};
+    bool dragging = false;
+
     while(!WindowShouldClose()){
 
         float deltaTime = GetFrameTime();
@@ -57,10 +64,34 @@ int main(){
         if(IsKeyDown(KEY_LEFT)) player.position.x -= player.speed * deltaTime;
         if(IsKeyDown(KEY_UP)) player.position.y -= player.speed * deltaTime;
         if(IsKeyDown(KEY_DOWN)) player.position.y += player.speed * deltaTime;
+        if (IsKeyPressed(KEY_Q)) cam.zoom *= 1.1f;
+        if (IsKeyPressed(KEY_E)) cam.zoom *= 0.9f;
 
-        cam.position.x = player.position.x;
-        cam.position.y = player.position.y;
+        cam.zoom = Clamp(cam.zoom, 0.3f, 4.0f);
 
+        Vector2 mouse = GetMousePosition();
+
+        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+            dragging = true;
+            lastMousePos = mouse;
+        }
+
+        if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) {
+            dragging = false;
+        }
+
+        if (dragging) {
+            Vector2 delta = Vector2Subtract(mouse, lastMousePos);
+
+            cam.position.x -= delta.x / cam.zoom;
+            cam.position.y -= delta.y / cam.zoom;
+
+            lastMousePos = mouse;
+        }
+        else{
+            cam.position.x = player.position.x;
+            cam.position.y = player.position.y;
+        }
         BeginDrawing();
 
         ClearBackground(BLACK);
